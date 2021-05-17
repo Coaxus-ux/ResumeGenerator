6711b3e8960336cb5f185fcbefda95c140af0a53
Coaxus-ux <zax.trujillo@gmail.com>
2021-05-17 07:45:37 -0500

    Responsive home

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const home = document.querySelector('#Home');
+home.setAttribute("style", `min-height: ${sizeWindow()}px`,);
+function sizeWindow() {
+  var sizeWindow = [0, 0];
+  if (typeof window.innerWidth != "undefined") {
+    sizeWindow = [window.innerWidth, window.innerHeight];
+  } else if (
+    typeof document.documentElement != "undefined" &&
+    typeof document.documentElement.clientWidth != "undefined" &&
+    document.documentElement.clientWidth != 0
+  ) {
+    sizeWindow = [
+      document.documentElement.clientWidth,
+      document.documentElement.clientHeight,
+    ];
+  } else {
+    sizeWindow = [
+      document.getElementsByTagName("body")[0].clientWidth,
+      document.getElementsByTagName("body")[0].clientHeight,
+    ];
+  }
+  return sizeWindow[1];
+}
+console.log(home);
