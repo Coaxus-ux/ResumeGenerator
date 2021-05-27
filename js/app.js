db461adb0d4bb5e4ab55e0b42af9c7482f05422f
coaxus-ux <trujillojulian.024@gmail.com>
2021-05-27 17:30:35 -0500

    backEnd is starting

--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
-const home = document.querySelector('#Home');
-let today = new Date().toISOString().substr(0, 10);
-document.querySelector("#dateForms").value = today;
+const home = document.querySelector("#Home"),
+  buttonLetsGo = document.querySelector("#finishButton");
 
-home.setAttribute("style", `min-height: ${sizeWindow()}px`,);
+let today = new Date().toISOString().substr(0, 10);
+
+document.querySelector("#dateFormsEducationStartCero").value = today;
+document.querySelector("#dateFormsEducationStartOne").value = today;
+document.querySelector("#dateFormsEducationFinishCero").value = today;
+document.querySelector("#dateFormsEducationFinishOne").value = today;
+
+document.querySelector("#dateFormsJobStartCero").value = today;
+document.querySelector("#dateFormsJobFinishCero").value = today;
+document.querySelector("#dateFormsJobStartOne").value = today;
+document.querySelector("#dateFormsJobFinishOne").value = today;
+
+buttonLetsGo.addEventListener("click", () => {
+  const userData = {
+    name: document.querySelector("#name").value,
+    proffession: document.querySelector("#proffession").value,
+    email: document.querySelector("#email").value,
+    phone: document.querySelector("#phone").value,
+    city: document.querySelector("#city").value,
+    country: document.querySelector("#country").value,
+    address: document.querySelector("#address").value,
+    aboutUser: document.querySelector("#aboutUser").value
+  };
+  const userEducation = {
+    universityOne: document.querySelector("#universityOne").value,
+    universityTwo: document.querySelector("#universityTwo").value,
+    degreeOne: document.querySelector("#degreeOne").value,
+    degreeTwo: document.querySelector("#degreeTwo").value,
+    dateUniversityStartCero: document.querySelector("#dateFormsEducationStartCero").value,
+    dateUniversityStartOne: document.querySelector("#dateFormsEducationStartOne").value,
+    dateUniversityFinishCero: document.querySelector("#dateFormsEducationFinishCero").value,
+    dateUniversityFinishOne: document.querySelector("#dateFormsEducationFinishCero").value
+  };
+  console.log(userData);
+  console.log(userEducation)
+});
+
+//home.setAttribute("style", `min-height: ${sizeWindow()}px`,);
 function sizeWindow() {
   var sizeWindow = [0, 0];
   if (typeof window.innerWidth != "undefined") {
